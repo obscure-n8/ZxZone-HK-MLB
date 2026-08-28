241a2bd89814eb6c3495af30845b6d5642821065
obscure <sheikharifinae4@gmail.com>
2026-08-28 18:09:43 +0600

    Update __init__.py

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,1 +1,7 @@
 # Database modules
+from bot.database.db import db
+from bot.database.users import users_db
+from bot.database.tasks import tasks_db
+from bot.database.settings import settings_db
+
+__all__ = ['db', 'users_db', 'tasks_db', 'settings_db']
